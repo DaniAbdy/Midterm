6c8451bd6315afee80ec3dca69d60b764901d798
madzhunova <madzhunova_a@auca.kg>
2021-11-22 14:50:12 +0600

    Abdyvasiev Daniyar First Commit

---
+++ app/src/main/java/kg/test/mymidterm/MainMidterm2.kt
@@ -0,0 +1,11 @@
+package kg.test.mymidterm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMidterm2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_midterm2)
+    }
+}
