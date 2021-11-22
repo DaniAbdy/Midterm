86e4eb6a5d4d3024bb2d8be9a9f175f344b1487b
madzhunova <madzhunova_a@auca.kg>
2021-11-22 15:11:36 +0600

    Abdyvasiev Daniyar Final Commit

--- app/src/main/java/kg/test/mymidterm/MainActivity.kt
+++ app/src/main/java/kg/test/mymidterm/MainActivity.kt
@@ -1,11 +1,18 @@
 package kg.test.mymidterm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<ImageView>(R.id.imageView9)
+
+        textView.setOnClickListener{
+            startActivity(Intent(this,MainMidterm2::class.java))
+        }
     }
 }
